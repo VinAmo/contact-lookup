4603a7277b3594e401524f736b38bdc9e55a645d
Kion Zheng <qingyang.k.zheng@gmail.com>
2023-08-25 20:43:57 +1000

    Prepare contact data and types for table

---
+++ src/types/index.tsx
@@ -0,0 +1,13 @@
+export interface TableHeaderData {
+  id: string;
+  name: string;
+}
+
+export interface TableRowData {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  avatarImage: string;
+}
